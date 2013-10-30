0a8494c92835f93e1ff3a477e0832b9acab030bc
AM-lvl3 <drewmorris@live.com>
2013-10-30 13:47:09 -0400

    done 05_dilly_blocks.rb

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -15,16 +15,33 @@ def adder(num=1, &block)
 	block.call + num
 end
 
-def repeater(n=1, &block)
-n = block.call
-n do |n|
-	
-	n+=1
+def repeater(n = 1)
+
+if n==0
+	yield
+
+else 
+(n).times do
+	yield
+	end
 end
 
-
-
-
 end
 
+  # describe "repeater" do
+  #   it "executes the default block" do
+  #     n = 2
+  #     repeater do
+  #       n += 3
+  #     end
+  #     n.should == 5
+  #   end
+
+  #   it "executes the default block 3 times" do
+  #     n = 0
+  #     repeater(3) do
+  #       n += 1
+  #     end
+  #     n.should == 3
+  #   end
 	
